d212609440975c399b9fa06cd2ff117b308f6abb
pablomarcel <pablomarcel@outlook.com>
2022-03-10 18:58:25 -0800

    completed hello-darkness.js

---
+++ hello-darkness.js
@@ -0,0 +1,13 @@
+const bodyEl = document.getElementsByTagName('body')[0]
+
+let colorVal =255
+const interval =100
+
+const bgColorDim = setInterval(()=>{
+    if (colorVal>0){
+        bodyEl.style.backgroundColor=`rgb(${colorVal},${colorVal},${colorVal})`
+        colorVal--
+    }else(
+        clearInterval(bgColorDim)
+        )
+}, interval)
--- remove-notice.js
+++ remove-notice.js
@@ -4,3 +4,20 @@ setTimeout(()=>{
     notice.style.display='none'
 
 }, 5000)
+
+//
+// let secondsRemaining =15
+// let countdownInterval=setInterval(function (){
+//     secondsRemaining--
+//     console.log(secondsRemaining)
+// },1000)
+
+let secondsRemaining =5
+let countdownInterval = setInterval(function () {
+    if (secondsRemaining > 0) {
+        secondsRemaining--
+        console.log(secondsRemaining)
+    } else {
+        clearInterval(countdownInterval)
+    }
+},1000)
