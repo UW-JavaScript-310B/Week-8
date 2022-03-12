f7b305cd15f9dfebdba8fe2e5926bcd7ffbaa9d3
Admin <admin@Admins-MacBook-Pro.local>
2022-03-11 22:29:31 -0800

    Set up fetch call to NYT API

--- best-books.js
+++ best-books.js
@@ -2,6 +2,10 @@ const formEl = document.getElementById('best-books-form');
 const yearEl = document.getElementById('year');
 const monthEl = document.getElementById('month');
 const dateEl = document.getElementById('date');
+const container = document.getElementById('books-container');
+
+const BASE_URL = `https://api.nytimes.com/svc/books/v3/lists/`;
+const key = `?&api-key=${API_KEY}`;
 
 formEl.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -10,5 +14,22 @@ formEl.addEventListener('submit', function(e) {
   const month = monthEl.value;
   const date = dateEl.value;
 
-  // Fetch bestselling books for date and add top 5 to page
+  fetch(`${BASE_URL}${year}-${month}-${date}/hardcover-fiction.json${key}`)
+  .then(data => data.json())
+  .then(response => {
+    console.log(response);
+    response.results.books.forEach(book => {
+      let title = document.createElement('p');
+      title.innerText = `Title: ${book.title.toLowerCase()}`;
+      container.appendChild(title);
+
+      let author = document.createElement('p');
+      author.innerText = `Author: ${book.author.toLowerCase()}`;
+      container.appendChild(author);
+
+      let description = document.createElement('p');
+      description.innerText = `Description: ${book.description.toLowerCase()}`;
+      container.appendChild(description);
+    })
+  })
 });
