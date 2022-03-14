94301e8b45b082a9cb9b70f96dcab112a0d6905c
Admin <admin@Admins-MacBook-Pro.local>
2022-03-13 23:16:35 -0700

    Updates to page layout. Connecting to randomuser API.

--- best-books.js
+++ best-books.js
@@ -7,26 +7,25 @@ const container = document.getElementById('books-container');
 const BASE_URL = `https://api.nytimes.com/svc/books/v3/lists/`;
 const key = `?&api-key=${API_KEY}`;
 
-fetch(`${BASE_URL}${year}-${month}-${date}/hardcover-fiction.json${key}`)
-  .then(data => data.json())
-  .then(response => {
-    console.log(response);
-    response.results.books.forEach(book => {
-      let title = document.createElement('p');
+// fetch(`${BASE_URL}${year}-${month}-${date}/hardcover-fiction.json${key}`)
+// .then(data => data.json())
+// .then(response => {
+//   console.log(response);
+//   response.results.books.forEach(book => {
+//     let title = document.createElement('p');
 
-      title.innerText = `Title: ${titleCase(book.title)}`;
-      container.appendChild(title);
+//     title.innerText = `Title: ${titleCase(book.title)}`;
+//     container.appendChild(title);
 
-      let author = document.createElement('p');
-      author.innerText = `Author: ${titleCase(book.author)}`;
-      container.appendChild(author);
+//     let author = document.createElement('p');
+//     author.innerText = `Author: ${titleCase(book.author)}`;
+//     container.appendChild(author);
 
-      let description = document.createElement('p');
-      description.innerText = `Description: ${sentenceCase(book.description)}`;
-      container.appendChild(description);
-    })
-  })
-});
+//     let description = document.createElement('p');
+//     description.innerText = `Description: ${sentenceCase(book.description)}`;
+//     container.appendChild(description);
+//   })
+// });
 
 function titleCase(str) {
   return (str.toLowerCase().split(' ')).map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
@@ -36,3 +35,20 @@ function sentenceCase(str) {
   str = str.toLowerCase();
   return str[0].toUpperCase() + str.slice(1);
 }
+
+const reviews = document.getElementsByClassName('review');
+
+fetch('https://randomuser.me/api/?results=3')
+.then(data => data.json())
+.then(response => {
+  response.results.forEach((person, index) => {
+    console.log(person);
+    const photo = document.createElement('img');
+    photo.src = person.picture.medium;
+    reviews[index].prepend(photo);
+
+    const name = document.createElement('h4');
+    name.innerText = `${person.name.first} ${person.name.last}`;
+    reviews[index].append(name);
+  })
+});
