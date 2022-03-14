ec47efa2b22958a76b9af8bee44b24320cf1ec2b
pablomarcel <pablomarcel@outlook.com>
2022-03-13 20:55:05 -0700

    final commit. cleaned up, deleted comments

--- best-books.js
+++ best-books.js
@@ -2,10 +2,7 @@ const formEl = document.getElementById('best-books-form');
 const yearEl = document.getElementById('year');
 const monthEl = document.getElementById('month');
 const dateEl = document.getElementById('date');
-
-
 const BASE_URL = 'https://api.nytimes.com/svc/books/v3/';
-
 const API_KEY ='mGqS9lCf3m3v6lc7FKR9rLCBcHxAMo0f'
 
 formEl.addEventListener('submit', function(e) {
@@ -20,6 +17,7 @@ formEl.addEventListener('submit', function(e) {
   const date = dateEl.value;
 
   // Fetch bestselling books for date and add top 5 to page
+  // Best hardcover fiction books for the date
 
   const url = `${BASE_URL}lists/${year}-${month}-${date}/hardcover-fiction.json?api-key=${API_KEY}`;
 
@@ -30,23 +28,6 @@ formEl.addEventListener('submit', function(e) {
       })
       .then(function(responseJson) {
         console.log(responseJson);
-
-        //need to loop through the top five. id est, from element 0 to 4
-        //need to create the elements dynamically
-
-        // let book = responseJson.results.books[0].title
-        // let author_ = responseJson.results.books[0].author
-        // let description = responseJson.results.books[0].description
-        // let book_img = responseJson.results.books[0].book_image
-        // let book_link = responseJson.results.books[0].amazon_product_url
-        // document.getElementById('book-title').innerText = book;
-        // document.getElementById('book-author').innerText = author_;
-        // document.getElementById('book-snippet').innerText = description;
-        // document.getElementById('book-link').href=book_link
-        // if (book_img.length > 0) {
-        //   const imgUrl = book_img;
-        //   document.getElementById('book-img').src = imgUrl;
-        // }
 
         for (let counter=0; counter<5; counter++){
 
@@ -64,7 +45,6 @@ formEl.addEventListener('submit', function(e) {
           if (book_img.length > 0) {
             const imgUrl = book_img;
             imgElem.src=imgUrl
-            //document.getElementById('book-img').src = imgUrl;
           }
 
           h1Elem.innerText=book
