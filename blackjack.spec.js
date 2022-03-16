96ffd35b0975d0309b6e0c362e3fd8a004049d99
Khanh Hoang <khanhqhoang@hotmail.com>
2022-03-15 23:00:50 -0700

    Khanh's assignment 8 resubmission

--- blackjack.spec.js
+++ blackjack.spec.js
@@ -3,11 +3,11 @@
 
 describe('DealerShouldDraw', function () {
     // build data structure for test cases
-    const case1 = [{suit: 'hearts', val: 10, displayVal: '10'}, {suit: 'clubs', val: 9, displayVal: '9'}];   
-    const case2 = [{suit: 'diamonds', val: 11, displayVal: 'Ace'}, {suit: 'spades', val: 6, displayVal: '6'}];
-    const case3 = [{suit: 'spades', val: 10, displayVal: '10'}, {suit: 'clubs', val: 6, displayVal: '6'},{suit: 'hearts', val: 11, displayVal: 'Ace'}];   
-    const case4 = [{suit: 'hearts', val: 2, displayVal: '2'}, {suit: 'spades', val: 4, displayVal: '4'},{suit: 'diamonds', val: 2, displayVal: '2'}, {suit: 'clubs', val: 5, displayVal: '5'}];
-    
+    const case1 = [{ suit: 'hearts', val: 10, displayVal: '10' }, { suit: 'clubs', val: 9, displayVal: '9' }];
+    const case2 = [{ suit: 'diamonds', val: 11, displayVal: 'Ace' }, { suit: 'spades', val: 6, displayVal: '6' }];
+    const case3 = [{ suit: 'spades', val: 10, displayVal: '10' }, { suit: 'clubs', val: 6, displayVal: '6' }, { suit: 'hearts', val: 11, displayVal: 'Ace' }];
+    const case4 = [{ suit: 'hearts', val: 2, displayVal: '2' }, { suit: 'spades', val: 4, displayVal: '4' }, { suit: 'diamonds', val: 2, displayVal: '2' }, { suit: 'clubs', val: 5, displayVal: '5' }];
+
     it('Determine if Dealer should draw another card - test case 1 - expect false', function () {
         const val = dealerShouldDraw(case1);
         expect(val).toBe(false);
@@ -24,5 +24,5 @@ describe('DealerShouldDraw', function () {
         const val = dealerShouldDraw(case4);
         expect(val).toBe(true);
     });
-    }
+}
 );
