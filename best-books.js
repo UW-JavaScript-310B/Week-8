cc8b382205be81aac92169aeb40ba5bb8cf705aa
Nell B <nellb28@yahoo.com>
2022-03-18 21:19:34 -0500

    Initial changes for best books

--- best-books.js
+++ best-books.js
@@ -1,9 +1,15 @@
-const formEl = document.getElementById('best-books-form');
-const yearEl = document.getElementById('year');
-const monthEl = document.getElementById('month');
-const dateEl = document.getElementById('date');
+// create api-key.js file with const API_KEY="your_api_key" in this same directory to use
+const BASE_URL =
+  "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json";
+const API_KEY = "Ta7hDm6AnnzMdHOXkjTT76x6OgLhPoQO";
+const url = `${BASE_URL}?api-key=${API_KEY}`;
 
-formEl.addEventListener('submit', function(e) {
+const formEl = document.getElementById("best-books-form");
+const yearEl = document.getElementById("year");
+const monthEl = document.getElementById("month");
+const dateEl = document.getElementById("date");
+
+formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const year = yearEl.value;
@@ -12,3 +18,24 @@ formEl.addEventListener('submit', function(e) {
 
   // Fetch bestselling books for date and add top 5 to page
 });
+console.log(url);
+
+fetch(url)
+  .then(function (data) {
+    return data.json();
+  })
+  .then(function (responseJson) {
+    console.log(responseJson);
+
+    let book = responseJson.results.books[0].title;
+    console.log("book = " + book);
+
+    const mainHeadline = article.headline.main;
+    console.log(mainHeadline);
+    document.getElementById("article-title").innerText = mainHeadline;
+
+    if (article.multimedia.length > 0) {
+      const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
+      document.getElementById("article-img").src = imgUrl;
+    }
+  });
