89a743368c9e6d8b7e8a34cbde42e6c5feb5e8e5
Nell B <nellb28@yahoo.com>
2022-03-19 00:30:16 -0500

    Cleaning up hello darkness assignment

--- hello-darkness.js
+++ hello-darkness.js
@@ -1,26 +1,3 @@
-/*
-function logTimeoutCalled() {
-  let x = 255;
-  if (x >= 0) {
-    x--;
-    document.body.style.backgroundColor = "rgb(x, x, x)";
-  } else {
-      clearInterval();
-    }
-}
-
-let myTimeout = setTimeout(logTimeoutCalled, 100);
-clearTimeout(myTimeout);
-function decreaseTime(secondsRemaining) {
-    console.log(secondsRemaining);
-    secondsRemaining--;
-}
-let secondsRemaining = 15;
-// Change time remaining every 1 second
-let countdownInterval = setInterval(decreaseTime(secondsRemaining), 1000);
-
-*/
-//TODO fix to your method
 const bodyEl = document.getElementsByTagName("body")[0];
 
 let colorVal = 255;
