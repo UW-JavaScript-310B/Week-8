2785bd4f2caf6a2968e7c6fd560402edf1ee57f3
Nell B <nellb28@yahoo.com>
2022-03-19 00:26:18 -0500

    Doing fetch in-class assignment

--- fetch.js
+++ fetch.js
@@ -1,23 +1,29 @@
 // create api-key.js file with const API_KEY="your_api_key" in this same directory to use
-const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-
-const url = `${BASE_URL}?q=tech&api-key=${API_KEY}`;
+const BASE_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+const url = `${BASE_URL}?q=cars&api-key=${API_KEY}`;
 
 fetch(url)
-  .then(function(data) {
+  .then(function (data) {
     return data.json();
   })
-  .then(function(responseJson) {
+  .then(function (responseJson) {
     console.log(responseJson);
 
     let article = responseJson.response.docs[0];
-    console.log(article);
-
-    const mainHeadline = article.headline.main;
-    document.getElementById('article-title').innerText = mainHeadline;
-
+    let articleEl = document.getElementById("article-link");
+    web_url = article.web_url;
+    articleEl.setAttribute("href", web_url);
     if (article.multimedia.length > 0) {
       const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
-      document.getElementById('article-img').src = imgUrl;
+      document.getElementById("article-img").src = imgUrl;
     }
+
+    let container = document.getElementsByClassName("container")[0];
+    let snippet = article.snippet;
+    let snippetDiv = document.createElement("div");
+    container.appendChild(snippetDiv);
+    snippetDiv.innerText = snippet;
+    let leadParagraphDiv = document.createElement("div");
+    container.appendChild(leadParagraphDiv);
+    leadParagraphDiv.innerHTML = article.lead_paragraph;
   });
