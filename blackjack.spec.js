fed38403cee2a1d4328951d50c97e1dddc8ee3a9
Sonal Gupta <sonalaggarwal555@gmail.com>
2022-03-27 19:57:30 -0700

    Week 8 Assignment Update

---
+++ blackjack.spec.js
@@ -0,0 +1,90 @@
+//This is test suite
+describe("Black Jack Test Suite", function () {
+
+    it("Test Case 1", function () {
+        let dealerHand = [
+            {
+                displayVal: '10',
+                val: 10,
+                suite: 'hearts'
+            },
+            {
+                displayVal: '9',
+                val: 9,
+                suite: 'hearts'
+            }
+        ];
+
+        expect(dealerShouldDraw(dealerHand)).toEqual(false);
+    });
+
+    it("Test Case 2", function () {
+        let dealerHand = [
+            {
+                displayVal: 'Ace',
+                val: 11,
+                suite: 'hearts'
+            },
+            {
+                displayVal: '6',
+                val: 6,
+                suite: 'hearts'
+            }
+        ];
+
+        expect(dealerShouldDraw(dealerHand)).toEqual(true);
+    });
+
+    it("Test Case 3", function () {
+        let dealerHand = [
+            {
+                displayVal: 'Ace',
+                val: 11,
+                suite: 'hearts'
+            },
+            {
+                displayVal: '10',
+                val: 10,
+                suite: 'hearts'
+            },
+            {
+                displayVal: '6',
+                val: 6,
+                suite: 'hearts'
+            }
+        ];
+
+        expect(dealerShouldDraw(dealerHand)).toEqual(false);
+    });
+
+    it("Test Case 4", function () {
+        let dealerHand = [
+            {
+                displayVal: '2',
+                val: 2,
+                suite: 'hearts'
+            },
+            {
+                displayVal: '4',
+                val: 4,
+                suite: 'hearts'
+            },
+            {
+                displayVal: '2',
+                val: 2,
+                suite: 'spades'
+            },
+            {
+                displayVal: '5',
+                val: 5,
+                suite: 'hearts'
+            }
+        ];
+
+        expect(dealerShouldDraw(dealerHand)).toEqual(true);
+    });
+
+
+});
+
+
