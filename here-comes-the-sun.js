3ea3ebb882a87012dba947ef1d6734906a774f2a
Sonal Gupta <sonalaggarwal555@gmail.com>
2022-03-28 22:06:27 -0700

    Week 8 Assignment Corrections

--- here-comes-the-sun.js
+++ here-comes-the-sun.js
@@ -1,6 +1,18 @@
+$(document).ready(function () {
 
-function change() {
-  var doc = document.getElementById("body-animate");
-  doc.style.backgroundColor = "rgb(255, 255, 255)";
-}
-setInterval(change, 4000);
+    var animationFrameId;
+    var color = 0;
+
+    var changeBackground = () => {
+        if (color >= 255) {
+            cancelAnimationFrame(animationFrameId);
+            return;
+        }
+
+        color++;
+        $('body').css('background-color', `rgb(${color}, ${color}, ${color})`);
+        animationFrameId = requestAnimationFrame(changeBackground);
+    };
+
+    animationFrameId = requestAnimationFrame(changeBackground);
+});
